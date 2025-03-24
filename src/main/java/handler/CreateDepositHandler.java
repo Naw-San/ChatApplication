6046b68eebe47de35943a5a2befa8dd801c72069
Naw-San <112665148+Naw-San@users.noreply.github.com>
2025-03-24 10:32:31 -0700

    Update CreateDepositHandler.java

--- src/main/java/handler/CreateDepositHandler.java
+++ src/main/java/handler/CreateDepositHandler.java
@@ -51,43 +51,6 @@ public class CreateDepositHandler implements BaseHandler {
                 .setBody(GsonTool.gson.toJson(response))
                 .build();
 
-        /*
-        TransactionDto transaction = GsonTool.gson.fromJson(request.getBody(), TransactionDto.class);
-
-        if (transaction.getUniqueId() == null || transaction.getUniqueId().isEmpty()) {
-            transaction.setUniqueId(UUID.randomUUID().toString());
-        }
-
-        if (transaction.getTransactionType() != null && transaction.getTransactionType() != TransactionType.Deposit)
-        {
-            return new ResponseBuilder()
-                    .setStatus("400 Bad Request")
-                    .setBody("Invalid transaction type for this endpoint.")
-                    .build();
-        }
-
-
-        UserDto user = UserDao.getInstance().get(transaction.getUserId());
-        if (user == null) {
-            return new ResponseBuilder()
-                    .setStatus("HTTP/1.1 200 OK")// 404 Not Found
-                    .setBody("User not found.")
-                    .build();
-        }
-
-        user.setBalance(user.getBalance() + transaction.getAmount());
-        UserDao.getInstance().put(user);
-        TransactionDao.getInstance().put(transaction);
-
-        RestApiAppResponse<TransactionDto> response = new RestApiAppResponse<>(true, List.of(transaction),
-                "Deposit created successfully.");
-        return new ResponseBuilder()
-                .setStatus("HTTP/1.1 200 OK") // 200 OK
-                .setBody(GsonTool.gson.toJson(response))
-                .build();
-
-         **/
-
     }
 
 }
